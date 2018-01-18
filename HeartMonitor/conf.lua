5d7e9fb4b68cf86a542f0e9282d95d91de37ad5d
Orb <orb@orbatron.org>
2018-01-18 03:12:54 +0000

    comments in conf.lua

--- HeartMonitor/conf.lua
+++ HeartMonitor/conf.lua
@@ -1,5 +1,5 @@
 function love.conf(t)
-	t.window.height = 400
+	t.window.height = 400 --setting this to fit into 800x600 resolution configured on orbpi.local
 	t.window.width = 500
-	t.window.vsync = true
+	t.window.vsync = true --toggling vsync to test hz vs fps adjustment
 end
