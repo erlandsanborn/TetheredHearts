413d765d881561b4f57f4243ffb8ef2ce301472f
erlandsanborn <erland.sanborn@gmail.com>
2018-04-12 01:46:18 +0000

    Another stable push

--- patient.love/conf.lua
+++ patient.love/conf.lua
@@ -1,5 +1,6 @@
 function love.conf(t)
 	t.window.width = 800
-	t.window.height = 450
+	t.window.height = 480
+	t.window.fullscreen = true
 	t.window.vsync = true --toggling vsync to test hz vs fps adjustment
 end
